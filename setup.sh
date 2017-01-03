3de2afe3f2dd3b433058dd3dd92dbe8066fb9367
mmscibor <mmscibor@gmail.com>
2017-01-02 20:51:13 -0500

    Trivial additions, including installation of cask, node, and spotify

---
+++ setup.sh
@@ -0,0 +1,12 @@
+echo "Installing Homebrew."
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap caskroom/cask
+
+echo "Installing core tools."
+brew install node
+
+
+echo "Installing 3rd party applications."
+export HOMEBREW_CASK_OPTS="--appdir=/Applications" # This sets the default installation directory for cask installed apps.
+
+brew cask install spotify
