79c293e6ff2a57cbf167a1c2ff4c34558374a2ab
mmscibor <mmscibor@gmail.com>
2017-01-03 21:32:46 -0500

    Added script to change shell to zsh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,11 @@ echo "Installing Homebrew."
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap caskroom/cask
 
+echo "Installing system tools."
+brew install zsh
+
+chsh -s $(which zsh)
+
 echo "Installing core tools."
 brew install node
 
